1f70a0d06baa1a890149ca8f7cd0492a1d628415
jessReyesM <l211130251@ganadero.tecnm.mx>
2024-11-27 09:10:40 -0600

    ✨Detour: Adjusting Game Window

--- phaserGame/src/views/AboutPage.vue
+++ phaserGame/src/views/AboutPage.vue
@@ -1,20 +1,17 @@
-<ion-page>
-  <ion-header>
-    <ion-toolbar>
-      
-      <ion-title>About</ion-title>
-    </ion-toolbar>
-  </ion-header>
-  <ion-content :fullscreen="true">
-    <ion-header collapse="condense">
+<template>
+  <ion-page>
+    <ion-header>
       <ion-toolbar>
-        
-        <ion-title size="large">About</ion-title>
+        <ion-title>About</ion-title>
       </ion-toolbar>
     </ion-header>
-
-    
-    <ExploreContainer name="About page" />
-  </ion-content>
-</ion-page>
+    <ion-content>
+      <ExploreContainer name="About page" />
+    </ion-content>
+  </ion-page>
 </template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
--- phaserGame/src/views/PlayPage.vue
+++ phaserGame/src/views/PlayPage.vue
@@ -5,17 +5,11 @@
         <ion-title>Play</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Play</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <PhaserContainer/>
+    <ion-content >
+        <PhaserContainer />
     </ion-content>
   </ion-page>
 </template>
-
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import PhaserContainer from '@/components/PhaserContainer.vue';

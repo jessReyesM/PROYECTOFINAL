31b15345dc62ab903a566eb4eb5e4f46bf101c1b
jessReyesM <l211130251@ganadero.tecnm.mx>
2024-11-27 09:38:17 -0600

    ✨Preloading Assets

--- phaserGame/src/game/PlayScene.js
+++ phaserGame/src/game/PlayScene.js
@@ -4,10 +4,38 @@ export class PlayScene extends Scene {
     constructor () {
       super({ key: 'PlayScene' })
     }
-    create () {
-        this.add.text(100, 100, "PlayScene", {
-            font: "24px Courier",
-            fill: "#ffffff",
-        });    
-    }
-  }
+
+    preload ()
+{
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('platform', 'assets/platform.png');
+    this.load.image('leftArrow', 'assets/leftarrow.png');
+    this.load.image('rightArrow', 'assets/rightarrow.png');
+    this.load.spritesheet('player', 
+        'assets/player.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+
+  create () {
+    // sets game values based on screen size
+    this.screenWidth = this.scale.width;
+    this.screenHeight = this.scale.height;
+    this.screenCenterX = this.screenWidth / 2;
+    this.controlsAreaHeight = this.screenHeight * 0.2;
+    this.gameAreaHeight = this.screenHeight - this.controlsAreaHeight;
+
+    // adds the player, platform, and controls
+    this.platform = this.physics.add.staticImage(0, this.gameAreaHeight, 'platform').setOrigin(0, 0).refreshBody();
+    this.player = this.physics.add.sprite(this.screenCenterX, this.gameAreaHeight - 24, 'player');
+    this.leftArrow = this.add.image(this.screenWidth * 0.1, this.gameAreaHeight + 40, 'leftArrow').setOrigin(0, 0).setInteractive()
+    this.rightArrow = this.add.image(this.screenWidth * 0.7, this.gameAreaHeight + 40, 'rightArrow').setOrigin(0, 0).setInteractive()
+}
+}
+
+this.platform = this.physics.add.staticImage(0, this.gameAreaHeight, 'platform').setOrigin(0, 0).refreshBody();
+this.player = this.physics.add.sprite(this.screenCenterX, this.gameAreaHeight - 24, 'player');
+this.leftArrow = this.add.image(this.screenWidth * 0.1, this.gameAreaHeight + 40, 'leftArrow').setOrigin(0, 0).setInteractive()
+this.rightArrow = this.add.image(this.screenWidth * 0.7, this.gameAreaHeight + 40, 'rightArrow').setOrigin(0, 0).setInteractive()

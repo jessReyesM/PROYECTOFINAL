2e4a76038837f9d6253a592dac6052b87d1d4380
jessReyesM <l211130251@ganadero.tecnm.mx>
2024-12-02 12:40:23 -0600

    ✨datos de puntuaciones

--- phaserGame/src/App.vue
+++ phaserGame/src/App.vue
@@ -9,9 +9,48 @@ import { IonApp, IonRouterOutlet } from '@ionic/vue';
 
 import { provide, ref} from 'vue';
 const gameScores = ref<number[]>([])
-const addGameScore = (score: number) => {
+  const addGameScore = (score: number) => {
+  if (gameScores.value.length >= 10) {
+    gameScores.value.shift()
+  }
+
   gameScores.value.push(score)
+
+  const scoreString = JSON.stringify(gameScores.value)
+
+  saveGameScores(scoreString)
 }
+
+const saveGameScores = async (scoreString: string) => {
+  if (Capacitor.isNativePlatform()) {
+    await Preferences.set({
+      key: 'gameScores',
+      value: scoreString
+    })
+  } else {
+    localStorage.setItem('gameScores', scoreString)
+  }
+}
+
+const loadGameScores = async () => {
+
+let savedScoreString = ''
+
+ if (Capacitor.isNativePlatform()) {
+  const result = await Preferences.get({ key: 'gameScores'})
+
+  savedScoreString = result.value || '';
+ } else {
+  savedScoreString = localStorage.getItem('gameScores') || '';
+ }
+
+ if (savedScoreString) {
+  gameScores.value = JSON.parse(savedScoreString)
+ }
+};
+
+loadGameScores();
+
 provide('gameScores', {
   gameScores, addGameScore
 })

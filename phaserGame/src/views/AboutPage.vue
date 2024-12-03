a236fa34cbc3be03e1fc9e14a755f81c890946e5
jessReyesM <l211130251@ganadero.tecnm.mx>
2024-12-03 12:19:44 -0600

    ✨cambio del texto en About

--- phaserGame/src/views/AboutPage.vue
+++ phaserGame/src/views/AboutPage.vue
@@ -6,7 +6,14 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <h1>This is a game built with Phaser, Vue, Ionic, and Capacitor!</h1>
+      <h1>¡Este es un juego construido con Phaser, Vue, Ionic y Capacitor!</h1>
+  <p>
+    ¡Bienvenido a esta emocionante aventura donde controlas a un valiente personaje a través de varios desafíos! </p>
+    <p>Encuentra estrellas para aumentar tu puntuación, evita las bombas peligrosas y consigue potenciadores como hongos para obtener habilidades especiales.  </p>
+   <p>Tu objetivo es sobrevivir el mayor tiempo posible mientras alcanzas la puntuación más alta. 
+    ¿Lograrás llegar a la cima de la tabla de clasificación?</p> 
+   
+ 
     </ion-content>
   </ion-page>
 </template>
